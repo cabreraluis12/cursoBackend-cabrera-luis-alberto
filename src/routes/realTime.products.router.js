0c2163b6e217eb0c5c9c756f39350ffa49ba1690
Luis Alberto Cabrera <cabreraluis1201@gmail.com>
2023-06-08 22:40:00 -0300

    fixed

--- src/routes/realTime.products.router.js
+++ src/routes/realTime.products.router.js
@@ -1,41 +1,52 @@
 import express from "express";
-import { productManager } from "../productManager.js";
+import { ProductModel } from "../Dao/models/products.models.js";
 
 export const routerRealTimeProducts = express.Router();
-const products = productManager.getProducts();
 
 export const initRealTimeProducts = (socketServer) => {
-    routerRealTimeProducts.get("/", (req, res) => {
-    return res.render("realTimeProducts", { products });
-    });
+  routerRealTimeProducts.get("/", async (req, res) => {
+    try {
+      const products = await ProductModel.find().exec();
+      res.render("realTimeProducts", { products });
+    } catch (error) {
+      res.status(500).send("Error retrieving products");
+    }
+  });
 
-
-socketServer.on("connection", (socket) => {
+  socketServer.on("connection", (socket) => {
     console.log("A user connected");
 
-    socket.emit("products", products);
+    // Emitir productos al cliente al conectar
+    ProductModel.find().exec()
+      .then((products) => {
+        socket.emit("products", products);
+      })
+      .catch((error) => {
+        console.error("Error retrieving products:", error);
+      });
 
-
-    socket.on("createProduct", (product) => {
-        products.push(product);
-
-    socketServer.emit("products", products);
+    socket.on("createProduct", async (product) => {
+      try {
+        const createdProduct = await ProductModel.create(product);
+        socketServer.emit("products", [createdProduct]);
+      } catch (error) {
+        console.error("Error creating product:", error);
+      }
     });
 
-    socket.on("deleteProduct", (productId) => {
-
-const index = products.findIndex((product) => product.id === productId);
-    if (index !== -1) {
-        products.splice(index, 1);
-
-        socketServer.emit("products", products);
-
-        console.log("Updated products list:", products);
-    }
+    socket.on("deleteProduct", async (productId) => {
+      try {
+        const deletedProduct = await ProductModel.findByIdAndDelete(productId).exec();
+        if (deletedProduct) {
+          socketServer.emit("products", [deletedProduct]);
+        }
+      } catch (error) {
+        console.error("Error deleting product:", error);
+      }
     });
 
     socket.on("disconnect", () => {
-        console.log("A user disconnected");
+      console.log("A user disconnected");
     });
-});
+  });
 };
